e6b68a2e89e8591c05dc3c5d7c41900fab70ac32
Rommel Rico <me@rommelrico.com>
2019-09-10 07:00:38 -0700

    :sparkles: Admin: Defining the User Admin and User Type Filter

--- pnk_server/users/admin.py
+++ pnk_server/users/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .forms import UserAdminForm
+from .choices import USER_TYPES
+
+
+class UserTypeFilter(admin.SimpleListFilter):
+    title = 'User Type'
+    parameter_name = 'User Type'
+
+    def lookups(self, request, model_admin):
+        return USER_TYPES
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(user_type__exact=self.value())
+        else:
+            return queryset
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
-    pass
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminForm
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'user_type',)
+    list_display = ('username', 'first_name', 'last_name', 'email', 'user_type',)
+    list_filter = (UserTypeFilter,)
+
+    class Meta:
+        model = User
+
