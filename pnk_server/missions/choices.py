390eef0cdc9723b41b174447cc6f62364c1095e2
Rommel Rico <me@rommelrico.com>
2019-09-11 21:40:52 -0700

    :sparkles: Missions Choices: Initial implementation

---
+++ pnk_server/missions/choices.py
@@ -0,0 +1,23 @@
+MISSION_CATEGORIES = (
+    ('HUN', 'Bounty Hunting'),
+    ('ENG', 'Engineering'),
+    ('EXP', 'Exploration'),
+    ('FRE', 'Freelancing'),
+    ('INF', 'Infiltration'),
+    ('PIR', 'Piracy'),
+    ('RES', 'Resources'),
+    ('SCO', 'Scouting'),
+    ('SEC', 'Security'),
+    ('SMU', 'Smuggling'),
+    ('SOC', 'Social'),
+    ('TRD', 'Trading'),
+    ('TRA', 'Training'),
+    ('TRN', 'Transport'),
+    ('XXX', 'Top Secret'),
+)
+
+MISSION_STATUSES = (
+    ('ACT', 'Active'),
+    ('FAI', 'Failed'),
+    ('SUC', 'Successful'),
+)
