4a74d81c0e864c9b30de73edfbeb5cd38c94ab8b
Rommel Rico <me@rommelrico.com>
2019-09-11 23:36:30 -0700

    :sparkles: Mission Serializer: Using the UserSerializer for users

--- pnk_server/missions/serializers.py
+++ pnk_server/missions/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Mission
+from ..users.serializers import UserSerializer
 
 
 class MissionSerializer(serializers.ModelSerializer):
+    commander = UserSerializer()
+    rsvp_users = UserSerializer(many=True)
+    attended_users = UserSerializer(many=True)
 
     class Meta:
         model = Mission
