3ae3e525f0db3e05d55ca96d771b59d64cd2540f
Rommel Rico <me@rommelrico.com>
2019-09-12 22:23:37 -0700

    :sparkles: Mission Serializers: Returning pks after all for users

--- pnk_server/missions/serializers.py
+++ pnk_server/missions/serializers.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
 from .models import Mission
-from ..users.serializers import UserSerializer
 
 
 class MissionSerializer(serializers.ModelSerializer):
-    commander = UserSerializer()
-    rsvp_users = UserSerializer(many=True)
-    attended_users = UserSerializer(many=True)
 
     class Meta:
         model = Mission
