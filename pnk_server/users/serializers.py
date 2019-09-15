4b1000fda2fa8fe98296c38c164159f396465c95
Rommel Rico <me@rommelrico.com>
2019-09-15 16:02:56 -0700

    :ambulance: Users Serializer: Removing the auth_token from the CreateUserSerializer
    Oops

--- pnk_server/users/serializers.py
+++ pnk_server/users/serializers.py
@@ -20,6 +20,5 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'auth_token', 'user_type',)
-        read_only_fields = ('auth_token',)
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'user_type',)
         extra_kwargs = {'password': {'write_only': True}}
